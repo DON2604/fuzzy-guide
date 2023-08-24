ff2cb1785b2a1bfc03102ee58ee72fb4535ec063
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-08-24 19:54:40 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 
-import { BrowserRouter , Route, Routes , Link } from 'react-router-dom';
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 
 
 import { Footer } from "./components/Footer";
-import { Ram } from "./components/Ram"
 import Home from './components/Home';
 import Courseplanner from './components/Courseplanner';
 import Aichat from './components/Aichat';
@@ -14,18 +13,13 @@ import Aichat from './components/Aichat';
 function App() {
   return (
     <BrowserRouter>
-      
         <NavBar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/course-planner" element={<Courseplanner />} />
           <Route exact path = "/Ai-chat" element={<Aichat />} />
-
         </Routes>
-        
         <Footer />
-        
-      
     </BrowserRouter>
   );
 }
