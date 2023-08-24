b2361827b5aef58014973ee3488c0138b6efb1c8
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-08-25 01:08:35 +0530

    added videos

--- Backend/routes/chatbot.js
+++ Backend/routes/chatbot.js
@@ -4,31 +4,26 @@ const fetchuser = require("../middleware/fetchuser");
 const { validationResult } = require("express-validator");
 
 router.post("/chat", fetchuser, async (req, res) => {
-  try {
-    //if errors, return bad request
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).send({ errors: errors.array() });
-    }
-    const url = 'https://chatgpt-api8.p.rapidapi.com/';
-    const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/json',
-		'X-RapidAPI-Key': '8c8d648c9emsh87341681f1dcf04p1633a4jsne18229bc72d1',
-		'X-RapidAPI-Host': 'chatgpt-api8.p.rapidapi.com'
-	},
-      body:
-        {
-          content: req.body.content,
-          role: req.body.role,
-        },
-    };
-    const response = await fetch(url, options);
-    const result = await response.json();
-    res.send(result);
+    try{
+  //if errors, return bad request
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).send({ errors: errors.array() });
+  // }
+  const url = 'https://chatgpt-api9.p.rapidapi.com/chat?question=How%20to%20learn%20python';
+  const options = {
+      method: 'GET',
+      headers: {
+          'X-RapidAPI-Key': '4f1d01abf6mshb3ecb561f6b70dcp128eafjsnad5a6d3d3228',
+          'X-RapidAPI-Host': 'chatgpt-api9.p.rapidapi.com'
+      }
+  };
+      const response = await fetch(url, options);
+      const result = await response.text();
+      res.json(result)
+      console.log(result);
   } catch (error) {
-    console.error(error);
+      console.error(error);
   }
 });
 
--- Backend/routes/planner.js
+++ Backend/routes/planner.js
@@ -21,12 +21,34 @@ router.get("/getplaylist", fetchuser, async (req, res) => {
     };
     const response = await fetch(url, options);
     const result = await response.json();
-    res.json(result);
+    res.send(result);
   } catch (error) {
     console.error(error);
   }
 });
 
-
+router.get("/getvideos", fetchuser, async (req, res) => {
+  try {
+    const { specificplaylist } = req.body;
+    //if errors, return bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+    const url = `https://youtube-v3-alternative.p.rapidapi.com/playlist?id=${specificplaylist}`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4f1d01abf6mshb3ecb561f6b70dcp128eafjsnad5a6d3d3228',
+		'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
+	}
+};
+	const response = await fetch(url, options);
+	const result = await response.json();
+  res.send(result);
+} catch (error) {
+	console.error(error);
+}
+});
 
 module.exports = router;
