0a4d99f7fbfac4a5c922f412b3b0f5b1c42d00e8
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-08-25 00:04:01 +0530

    done

--- Backend/routes/chatbot.js
+++ Backend/routes/chatbot.js
@@ -10,24 +10,23 @@ router.post("/chat", fetchuser, async (req, res) => {
     if (!errors.isEmpty()) {
       return res.status(400).send({ errors: errors.array() });
     }
-    const url = "https://chatgpt-api8.p.rapidapi.com/";
+    const url = 'https://chatgpt-api8.p.rapidapi.com/';
     const options = {
-      method: 'POST',
-      headers: {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": "4f1d01abf6mshb3ecb561f6b70dcp128eafjsnad5a6d3d3228",
-        "X-RapidAPI-Host": "chatgpt-api8.p.rapidapi.com",
-      },
-      body: [
+	method: 'POST',
+	headers: {
+		'content-type': 'application/json',
+		'X-RapidAPI-Key': '4f1d01abf6mshb3ecb561f6b70dcp128eafjsnad5a6d3d3228',
+		'X-RapidAPI-Host': 'chatgpt-api8.p.rapidapi.com'
+	},
+      body:
         {
           content: req.body.content,
           role: req.body.role,
         },
-      ],
     };
     const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
+    const result = await response.json();
+    res.send(result);
   } catch (error) {
     console.error(error);
   }
