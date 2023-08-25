c26db2597d357d2ca8e09b023572d01f7bee8b93
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-08-25 09:49:22 +0530

    ADDED STUFFS

--- Backend/routes/chatbot.js
+++ Backend/routes/chatbot.js
@@ -3,29 +3,29 @@ const router = express.Router();
 const fetchuser = require("../middleware/fetchuser");
 const { validationResult } = require("express-validator");
 
-router.post("/chat", fetchuser, async (req, res) => {
-    try{
-  //if errors, return bad request
-  // const errors = validationResult(req);
-  // if (!errors.isEmpty()) {
-  //   return res.status(400).send({ errors: errors.array() });
-  // }
-  const question = encodeURIComponent(req.body.content);
-  const url = `https://chat-gpt-ai-bot.p.rapidapi.com/GenerateAIWritter?prompt=${question}`;
-  const options = {
-      method: 'GET',
+router.get("/chat", fetchuser, async (req, res) => {
+  try {
+    //   if errors, return bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+    const question = encodeURIComponent(req.body.content);
+    const url = `https://chat-gpt-ai-bot.p.rapidapi.com/GenerateAIWritter?prompt=${question}`;
+    const options = {
+      method: "GET",
       headers: {
-          'X-RapidAPI-Key': '8c8d648c9emsh87341681f1dcf04p1633a4jsne18229bc72d1',
-          'X-RapidAPI-Host': 'chat-gpt-ai-bot.p.rapidapi.com'
+        "X-RapidAPI-Key": "8c8d648c9emsh87341681f1dcf04p1633a4jsne18229bc72d1",
+        "X-RapidAPI-Host": "chat-gpt-ai-bot.p.rapidapi.com",
       },
-  };
-      const response = await fetch(url, options);
-      const result = await response.text();
-      res.json(result)
-      console.log(result);
+    };
+    const response = await fetch(url, options);
+    const result = await response.text();
+    res.json(result);
+    console.log(result);
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred' });
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
--- src/components/AuthSection/Login.js
+++ src/components/AuthSection/Login.js
@@ -25,7 +25,7 @@ export default function Login(props) {
     if (json.success) {
       //refirect
       localStorage.setItem("token", json.authtoken);
-      navigate("/home");
+      navigate("/");
     } 
   };
 
