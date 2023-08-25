6ef7c7a5c47c2954acc6fb3d8719b9db58229c68
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-08-25 21:47:33 +0530

    upload

--- Backend/routes/chatbot.js
+++ Backend/routes/chatbot.js
@@ -15,7 +15,7 @@ router.post("/chat", fetchuser, async (req, res) => {
     const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "8c8d648c9emsh87341681f1dcf04p1633a4jsne18229bc72d1",
+        "X-RapidAPI-Key": "4f1d01abf6mshb3ecb561f6b70dcp128eafjsnad5a6d3d3228",
         "X-RapidAPI-Host": "chat-gpt-ai-bot.p.rapidapi.com",
       },
     };
