9d4132b4077763027a6586474904e0e17b95ece5
Swarnavo44 <swarnavomukherjee03@gmail.com>
2023-08-25 09:15:02 +0530

    change-chat

--- Backend/routes/chatbot.js
+++ Backend/routes/chatbot.js
@@ -10,13 +10,14 @@ router.post("/chat", fetchuser, async (req, res) => {
   // if (!errors.isEmpty()) {
   //   return res.status(400).send({ errors: errors.array() });
   // }
-  const url = 'https://chatgpt-api9.p.rapidapi.com/chat?question=How%20to%20learn%20python';
+  const question = encodeURIComponent(req.body.content);
+  const url = `https://chatgpt-api9.p.rapidapi.com/chat?question=${question}`;
   const options = {
       method: 'GET',
       headers: {
           'X-RapidAPI-Key': '4f1d01abf6mshb3ecb561f6b70dcp128eafjsnad5a6d3d3228',
           'X-RapidAPI-Host': 'chatgpt-api9.p.rapidapi.com'
-      }
+      },
   };
       const response = await fetch(url, options);
       const result = await response.text();
@@ -24,6 +25,7 @@ router.post("/chat", fetchuser, async (req, res) => {
       console.log(result);
   } catch (error) {
       console.error(error);
+      res.status(500).json({ error: 'An error occurred' });
   }
 });
 
